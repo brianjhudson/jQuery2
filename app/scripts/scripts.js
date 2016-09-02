a7e2f347ea1183197b7d18f578f427bdad411616
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-02 11:13:13 -0500

    Changed if statement in advanceTask to use case insensitive comparison

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   $('#newTaskForm').hide();
   function supports_html5_storage() {
     try {
@@ -9,64 +8,82 @@ $(document).ready(function() {
     }
   }
   var listo = [];
+  var localList = [];
 
   // Checks for local storage and repopulates list or creates new list;
   if (supports_html5_storage()) {
     var supportsStorage = true;
+    var listItemHTML;
     if (localStorage['listExists']) {
-        var counter = 0;
-        for (var prop in localStorage) {
-          listo[counter].task = prop;
-          listo[counter].id = localStorage[prop];
-          counter++;
+      var local = JSON.parse(localStorage['listo']);
+      for (var i = 0; i < local.length; i++) {
+        listo.push(local[i]);
+        if (local[i].id === 'new') {
+          $('#newList').append(
+            '<a href="#finish" class="" id="item">' +
+            '<li class="list-group-item">' +
+            '<h3>' + local[i].task + '</h3>' +
+            '<span class="arrow pull-right"></span></li></a>'
+          );
         }
-    }
-    else {
-      localStorage['listo'] = listo;
-      console.log(localStorage['listo']);
-    }
-  }
+        else if (local[i].id === 'inProgress') {
+          $('#currentList').append(
+            '<a href="#finish" class="" id="inProgress">' +
+            '<li class="list-group-item">' +
+            '<h3>' + local[i].task + '</h3>' +
+            '<span class="arrow pull-right"></span></li></a>'
+          );
+        }
+        else if (local[i].id === 'archived') {
+          $('#archivedList').append(
+            '<a href="#finish" class="" id="archived">' +
+            '<li class="list-group-item">' +
+            '<h3>' + local[i].task + '</h3>' +
+            '<span class="arrow pull-right"></span></li></a>'
 
+          );
+        }
+        // listGroup.append(
+      }
+    }
+  };
   var Task = function(task) {
     this.task = task;
     this.id = 'new';
   };
 
-  // Adds task to list
+//  Adds task to list
   var addTask = function(task) {
     if(task) {
       task = new Task(task);
       listo.push(task);
+      localStorage.setItem('listo', JSON.stringify(listo));
+      localStorage.setItem('listExists', 'true');
       $('#newItemInput').val('');
       $('#newList').append(
         '<a href="#finish" class="" id="item">' +
         '<li class="list-group-item">' +
         '<h3>' + task.task + '</h3>' +
-        '<span class="arrow pull-right">' +
-        '<i class="glyphicon glyphicon-arrow-right">' +
-        '</span></li></a>');
-      }
+        '<span class="arrow pull-right"></span></li></a>'
+      );
       $('#newTaskForm').slideToggle('fast', 'linear');
-      if (supportsStorage) {
-        localStorage[task.task] = task.id;
-        localStorage['listExists'] = "true";
-      }
-    };
-
+    }
+  };
   var advanceTask = function(task) {
-    var modified = task.innerText.trim();
+    var modified = task.innerText.trim().toLowerCase();
     for (var i = 0; i < listo.length; i++) {
-      if (listo[i].task === modified) {
-        if (listo[i].id = 'new') {
+      if (listo[i].task.toLowerCase() === modified) {
+        if (listo[i].id === 'new') {
           listo[i].id = 'inProgress';
-          localStorage[task.task] = "inProgress"
+          localStorage.setItem('listo', JSON.stringify(listo));
         }
-        else if (listo[i].id = 'inProgress') {
+        else if (listo[i].id === 'inProgress') {
           listo[i].id = 'archived';
-          localStorage[task.task] = "archived";
+          localStorage.setItem('listo', JSON.stringify(listo));
         }
         else {
           listo.splice(i, 1);
+          localStorage.setItem('listo', JSON.stringify(listo));
         }
         break;
       }
@@ -100,9 +117,9 @@ $(document).ready(function() {
   $(document).on('click', '#inProgress', function(e) {
     e.preventDefault();
     var task = this;
-    task.id = 'archived';
-    var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
     advanceTask(task);
+    this.id = 'archived';
+    var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
     $('#archivedList').append(changeIcon);
   });
 
